2f1af1f88dd59de97c44b6af3119c381ab7e4a9b
Nelson Pui <nelsonpui@hotmail.com>
2019-04-08 11:13:06 +0800

    Remove deployment from script and use azure devops

---
+++ vdc-networking-lab/vdc_script/DeployVDCNetworking.ps1
@@ -0,0 +1,20 @@
+﻿# User must be logged into their Azure account
+
+#As per https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-manager-cross-resource-group-deployment
+$firstRG = "VDC-Hub"
+$secondRG = "VDC-Spoke1"
+$thirdRG = "VDC-Spoke2"
+$fourthRG = "VDC-OnPrem"
+$fifthRG = "VDC-NVA"
+
+New-AzResourceGroup -Name $firstRG -Location australiaeast
+New-AzResourceGroup -Name $secondRG -Location australiaeast
+New-AzResourceGroup -Name $thirdRG -Location australiaeast
+New-AzResourceGroup -Name $fourthRG -Location australiaeast
+New-AzResourceGroup -Name $fifthRG -Location australiaeast
+
+<#New-AzResourceGroupDeployment `
+  -Name "VDC-Create" `
+  -ResourceGroupName $firstRG `
+  -TemplateUri "https://raw.githubusercontent.com/Araffe/vdc-networking-lab/master/VDC-Networking-Master.json"
+  #>
